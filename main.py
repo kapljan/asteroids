a292da3ff98fe899dd8deed76ae72499c7a8e485
Ermook <ermookx97x@gmail.com>
2024-12-17 23:11:19 +0100

    CH2.3 - added framerate cap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from constants import *
+from constants import * #wildcard import, not recomended to use, but alright for this project
 
 def main():
     print("Starting asteroids!")
@@ -10,6 +7,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time_clock = pygame.time.Clock()
+    dt = 0 #number of fps?
+
     while True:
         for event in pygame.event.get():
             #quits game if you press [X]
@@ -18,6 +18,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        #param 60 means the game can't run above 60 fps
+        dt = time_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
